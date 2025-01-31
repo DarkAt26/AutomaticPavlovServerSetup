e7ae8937304a40542cd7828353f9c7df65011231
DarkAt26 <84019236+DarkAt26@users.noreply.github.com>
2025-01-31 17:41:26 +0100

    #1

---
+++ Core.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticPavlovServerSetup
+{
+    internal class Core
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello PavServer");
+
+            RunCommandWithBash("sudo apt update && sudo apt install -y gdb curl lib32gcc1 libc++-dev unzip");
+
+            Console.WriteLine("Bye PavServer");
+        }
+
+
+        public static string RunCommandWithBash(string command)
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = "/bin/bash";
+            psi.Arguments = command;
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            using var process = Process.Start(psi);
+
+            process.WaitForExit();
+
+            var output = process.StandardOutput.ReadToEnd();
+
+            return output;
+        }
+    }
+}
