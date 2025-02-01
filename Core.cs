bc1ec1ac2396505662f506d0d7686cfae66459cf
DarkAt26 <84019236+DarkAt26@users.noreply.github.com>
2025-02-01 14:31:57 +0100

    #5

--- Core.cs
+++ Core.cs
@@ -4,112 +4,67 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using AutomaticPavlovServerSetup;
+using Newtonsoft.Json;
+
 
 namespace AutomaticPavlovServerSetup
 {
     internal class Core
     {
+        public static RootObject serverConfig = new RootObject();
         static void Main()
         {
-            Console.WriteLine("Hey");
+            Console.WriteLine("-AutomaticPavlovServerSetup-");
 
-            Command("sudo apt update && sudo apt install -y gdb curl lib32gcc1 libc++-dev unzip");
-            AddSteamUser("pwdSt3am");
-            Command("su - steam -c 'mkdir -p /home/steam/Steam && cd /home/steam/Steam && curl -sqL \"https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz\" | tar zxvf -'");
-            Command("echo \"pwdSt3am\" | sudo -S rm /usr/lib/x86_64-linux-gnu/libc++.so");
-            Command("echo \"pwdSt3am\" | sudo -S ln -s /usr/lib/x86_64-linux-gnu/libc++.so.1 /usr/lib/x86_64-linux-gnu/libc++.so");
-            //CommandSteam("sudo su -l steam -c \"/home/steam/Steam/steamcmd.sh +force_install_dir /home/steam/pavlovserver +login anonymous +app_update 622970 -beta shack +exit\"");
+            string configPath = Path.Combine(Directory.GetCurrentDirectory(), "APSS_SetupConfig.json");
 
-            Console.WriteLine(DateTime.UtcNow);
-
-            string steamCmdPath = "/home/steam/Steam/steamcmd.sh";
-            string arguments = "+force_install_dir /home/steam/pavlovserver +login anonymous +app_update 622970 -beta shack +quit";
-
-            // Run the command as the 'steam' user
-            ProcessStartInfo psi = new ProcessStartInfo
+            if (File.Exists(configPath))
             {
-                FileName = "/bin/bash",
-                Arguments = $"-c \"runuser -l steam -c '{steamCmdPath} {arguments}'\"",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-
-            // Start the process
-            using (Process process = new Process { StartInfo = psi })
+                serverConfig = JsonConvert.DeserializeObject<RootObject>(File.ReadAllText(configPath));
+            }
+            else
             {
-                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
-                process.ErrorDataReceived += (sender, e) => Console.WriteLine("ERROR: " + e.Data);
-
-                process.Start();
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
-                process.WaitForExit();  // Wait until the process completes
+                CreateFile(configPath, JsonConvert.SerializeObject(serverConfig, Formatting.Indented));
+                Console.WriteLine(JsonConvert.SerializeObject(serverConfig, Formatting.Indented));
             }
 
-            Console.WriteLine("SteamCMD process completed.");
-            Console.WriteLine(DateTime.UtcNow);
-
-            arguments = "+login anonymous +app_update 1007 +quit";
-
-            // Run the command as the 'steam' user
-            ProcessStartInfo psi2 = new ProcessStartInfo
+            if (JsonConvert.SerializeObject(new RootObject(), Formatting.Indented) == JsonConvert.SerializeObject(serverConfig, Formatting.Indented))
             {
-                FileName = "/bin/bash",
-                Arguments = $"-c \"runuser -l steam -c '{steamCmdPath} {arguments}'\"",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-
-            // Start the process
-            using (Process process = new Process { StartInfo = psi2 })
-            {
-                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
-                process.ErrorDataReceived += (sender, e) => Console.WriteLine("ERROR: " + e.Data);
-
-                process.Start();
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
-                process.WaitForExit();  // Wait until the process completes
+                Console.WriteLine("Failed. Setup APSS_SetupConfig.json");
+                return;
             }
 
-            Console.WriteLine("SteamCMD1007 process completed.");
-            Console.WriteLine(DateTime.UtcNow);
+            Console.WriteLine("InputCommand (Setup/Update): ");
+
+            string input = Console.ReadLine();
+
+            DateTime startTime = DateTime.UtcNow;
+
+            Console.WriteLine(startTime + " Starting...");
 
 
+            switch (input.ToLower())
+            {
+                case "setup":
+                    SetupServer();
+                    break;
 
+                case "update":
+                    UpdateServer();
+                    break;
+            }
+            
+            DateTime endTime = DateTime.UtcNow;
 
-            //Command("su - steam -c \"/home/steam/Steam/steamcmd.sh +login anonymous +app_update 1007 +quit\"");
-            Command("sudo -u steam mkdir -p /home/steam/.steam/sdk64");
-            Command("echo \"pwdSt3am\" | sudo -S rm /usr/lib/x86_64-linux-gnu/libc++.so");
-            Command("echo \"pwdSt3am\" | sudo -S ln -s /usr/lib/x86_64-linux-gnu/libc++.so.1 /usr/lib/x86_64-linux-gnu/libc++.so");
-            Command("sudo -u steam chmod +x /home/steam/pavlovserver/PavlovServer.sh");
+            TimeSpan time = endTime - startTime;
 
-            Command("sudo -u steam mkdir -p /home/steam/pavlovserver/Pavlov/Saved/Logs");
-            Command("sudo -u steam mkdir -p /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer");
-            Command("sudo -u steam mkdir -p /home/steam/pavlovserver/Pavlov/Saved/Mods");
-
-
-            int[] ports = { 7777, 8177, 9100 };
-
-            OpenPorts(ports);
-            DisableIPv6();
-
-            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/mods.txt", "");
-            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/whitelist.txt", "");
-            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt", "");
-
-            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini", "[/Script/Pavlov.DedicatedServer]\r\nbEnabled=true\r\nServerName=\"My_private_idaho\" \r\nMaxPlayers=10     #its recommended for the best experience to keep shack servers at or below 10 players pcvr and psvr are both capped at 24 too. \r\nApiKey=\"ABC123FALSEKEYDONTUSEME\"\r\nbSecured=true\r\nbCustomServer=true \r\nbVerboseLogging=false \r\nbCompetitive=false #This only works for SND\r\nbWhitelist=false \r\nRefreshListTime=120 \r\nLimitedAmmoType=0 \r\nTickRate=90\r\nTimeLimit=60\r\nAFKTimeLimit=300\r\n#Password=0000 \r\n#BalanceTableURL=\"vankruptgames/BalancingTable/main\"\r\nMapRotation=(MapId=\"UGC1758245796\", GameMode=\"GUN\")\r\nMapRotation=(MapId=\"datacenter\", GameMode=\"SND\")\r\nMapRotation=(MapId=\"sand\", GameMode=\"DM\")\r\nAdditionalMods=UGC3462586");
-
-            CreateFile("/etc/systemd/system/pavlovserver.service", "[Unit]\r\nDescription=Pavlov VR dedicated server\r\n\r\n[Service]\r\nType=simple\r\nWorkingDirectory=/home/steam/pavlovserver\r\nExecStart=/home/steam/pavlovserver/PavlovServer.sh\r\n\r\nRestartSec=1\r\nRestart=always\r\nUser=steam\r\nGroup=steam\r\n\r\n[Install]\r\nWantedBy = multi-user.target");
-
-            Console.WriteLine("Bye");
+            Console.WriteLine(endTime + " Done. Finished in " + time.Minutes + "min " + time.Seconds + "s.");
+            Console.WriteLine("-AutomaticPavlovServerSetup-");
         }
 
         public static void CreateDirectory(string directoryPath)
@@ -190,7 +145,7 @@ namespace AutomaticPavlovServerSetup
             Command("echo \"steam:" + password + "\" | sudo chpasswd");
         }
 
-        public static void OpenPorts(int[] ports)
+        public static void OpenPorts(List<int> ports)
         {
             Command("sudo ufw status");
             foreach (int port in ports)
@@ -213,5 +168,140 @@ namespace AutomaticPavlovServerSetup
 
             Console.WriteLine("File created and written successfully.");
         }
+
+        public static void SteamCMDCommand(string arguments)
+        {
+            Console.WriteLine(DateTime.UtcNow);
+
+            string steamCmdPath = "/home/steam/Steam/steamcmd.sh";
+
+            // Run the command as the 'steam' user
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c \"runuser -l steam -c '{steamCmdPath} {arguments}'\"",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+
+            // Start the process
+            using (Process process = new Process { StartInfo = psi })
+            {
+                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+                process.ErrorDataReceived += (sender, e) => Console.WriteLine("ERROR: " + e.Data);
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();  // Wait until the process completes
+            }
+
+            Console.WriteLine("SteamCMD process completed.");
+            Console.WriteLine(DateTime.UtcNow);
+        }
+
+        public static void SetupServer()
+        {
+            Command("sudo apt update && sudo apt install -y gdb curl lib32gcc1 libc++-dev unzip");
+            AddSteamUser(serverConfig.SteamPassword);
+            Command("su - steam -c 'mkdir -p /home/steam/Steam && cd /home/steam/Steam && curl -sqL \"https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz\" | tar zxvf -'");
+
+            Command("echo \"" + serverConfig.SteamPassword + "\" | sudo -S rm /usr/lib/x86_64-linux-gnu/libc++.so");
+            Command("echo \"" + serverConfig.SteamPassword + "\" | sudo -S ln -s /usr/lib/x86_64-linux-gnu/libc++.so.1 /usr/lib/x86_64-linux-gnu/libc++.so");
+
+
+            SteamCMDCommand("+force_install_dir /home/steam/pavlovserver +login anonymous +app_update 622970 " + serverConfig.Platform + " +quit");
+            SteamCMDCommand("+login anonymous +app_update 1007 +quit");
+
+
+            Command("sudo -u steam mkdir -p /home/steam/.steam/sdk64");
+
+            Command("echo \"" + serverConfig.SteamPassword + "\" | sudo -S rm /usr/lib/x86_64-linux-gnu/libc++.so");
+            Command("echo \"" + serverConfig.SteamPassword + "\" | sudo -S ln -s /usr/lib/x86_64-linux-gnu/libc++.so.1 /usr/lib/x86_64-linux-gnu/libc++.so");
+            
+            Command("sudo -u steam chmod +x /home/steam/pavlovserver/PavlovServer.sh");
+
+            Command("sudo -u steam mkdir -p /home/steam/pavlovserver/Pavlov/Saved/Logs");
+            Command("sudo -u steam mkdir -p /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer");
+            Command("sudo -u steam mkdir -p /home/steam/pavlovserver/Pavlov/Saved/Mods");
+
+
+            List<int> ports = new List<int>();
+            ports.AddRange(serverConfig.StandardPorts);
+            ports.Add(serverConfig.RconSettings.Port);
+            
+
+            OpenPorts(ports);
+            DisableIPv6();
+
+            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/mods.txt", string.Join("\r\n", serverConfig.Mods));
+            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/whitelist.txt", string.Join("\r\n", serverConfig.Whitelist));
+            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/blacklist.txt", string.Join("\r\n", serverConfig.Blacklist));
+
+            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt", "Password=" + serverConfig.RconSettings.Password + "\r\n" + "Port=" + serverConfig.RconSettings.Port);
+
+            string MapRotationString = "";
+            foreach (string s in serverConfig.ConfigSetup.MapRotation)
+            {
+                MapRotationString += "MapRotation=(MapId=\"" + s.Split(' ')[0] + "\", GameMode=\"" + s.Split(' ')[1] + "\")\r\n";
+            }
+
+            CreateFile("/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini", serverConfig.ConfigSetup.Config + "\r\n\r\n" + MapRotationString + string.Join("\r\nAdditionalMods=", serverConfig.ConfigSetup.AdditionalMods));
+
+            CreateFile("/etc/systemd/system/pavlovserver.service", serverConfig.PavlovService);
+
+            if (serverConfig.StartServerAfterCompletion)
+            {
+                Command("sudo systemctl start pavlovserver");
+            }
+        }
+    
+        public static void UpdateServer()
+        {
+            Command("sudo systemctl stop pavlovserver");
+
+            SteamCMDCommand("+force_install_dir /home/steam/pavlovserver +login anonymous +app_update 622970 " + serverConfig.Platform + " +quit");
+            SteamCMDCommand("+login anonymous +app_update 1007 +quit");
+
+            Command("sudo -u steam mkdir -p /home/steam/.steam/sdk64");
+
+            Command("echo \"" + serverConfig.SteamPassword + "\" | sudo -S rm /usr/lib/x86_64-linux-gnu/libc++.so");
+            Command("echo \"" + serverConfig.SteamPassword + "\" | sudo -S ln -s /usr/lib/x86_64-linux-gnu/libc++.so.1 /usr/lib/x86_64-linux-gnu/libc++.so");
+
+            Command("sudo systemctl start pavlovserver");
+        }
     }
+
+
+
+    public class RconSettings
+    {
+        public int Port { get; set; } = 9100;
+        public string Password { get; set; } = "";
+    }
+
+    public class ConfigSetup
+    {
+        public string Config { get; set; } = "[/Script/Pavlov.DedicatedServer]\nbEnabled=true\nServerName=\"AutomaticPavlovServerSetup\" \nMaxPlayers=10\nApiKey=\"ABC123FALSEKEYDONTUSEME\"\nbSecured=true\nbCustomServer=true \nbVerboseLogging=false \nbCompetitive=false\nbWhitelist=false \nRefreshListTime=120 \nLimitedAmmoType=0 \nTickRate=90\nTimeLimit=60\nAFKTimeLimit=300\n#Password=0000 \n#BalanceTableURL=\"vankruptgames/BalancingTable/main\"";
+        public List<string> MapRotation { get; set; } = new List<string> { "UGC1758245796 GUN", "datacenter SND", "sand DM" };
+        public List<string> AdditionalMods { get; set; } = new List<string> { "UGC3462586" };
+    }
+
+    public class RootObject
+    {
+        public List<int> StandardPorts { get; set; } = new List<int> { 7777, 8177 };
+        public RconSettings RconSettings { get; set; } = new RconSettings();
+        public List<string> Mods { get; set; } = new List<string>();
+        public List<string> Whitelist { get; set; } = new List<string>();
+        public List<string> Blacklist { get; set; } = new List<string>();
+        public string PavlovService { get; set; } = "[Unit]\nDescription=Pavlov VR dedicated server\n\n[Service]\nType=simple\nWorkingDirectory=/home/steam/pavlovserver\nExecStart=/home/steam/pavlovserver/PavlovServer.sh\n\nRestartSec=1\nRestart=always\nUser=steam\nGroup=steam\n\n[Install]\nWantedBy = multi-user.target";
+        public ConfigSetup ConfigSetup { get; set; } = new ConfigSetup();
+        public bool StartServerAfterCompletion { get; set; } = true;
+        public string SteamPassword { get; set; } = "pwdSt3am";
+        public string Platform { get; set; } = "-beta shack";
+    }
+
 }
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ```
+chmod +x AutomaticPavlovServerSetup && ./AutomaticPavlovServerSetup
+```
+
+```
+nano APSS_SetupConfig.json
+```
+
+```
 chmod +x AutomaticPavlovServerSetup
 ```
 ```
